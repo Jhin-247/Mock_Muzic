989d0bbb76ec5468ce08007e7f0c3152161980f0
Jhin-247 <anhtuana1408@gmail.com>
2022-08-05 13:24:33 +0700

    add <layout> to fragments

--- app/src/main/java/com/tuanna21/mockproject_tuanna21/screen/main/fragments/songfragments/subfragment/AlbumFragment.java
+++ app/src/main/java/com/tuanna21/mockproject_tuanna21/screen/main/fragments/songfragments/subfragment/AlbumFragment.java
@@ -1,4 +1,30 @@
 package com.tuanna21.mockproject_tuanna21.screen.main.fragments.songfragments.subfragment;
 
-public class AlbumFragment {
+import androidx.lifecycle.ViewModelProvider;
+
+import com.tuanna21.mockproject_tuanna21.R;
+import com.tuanna21.mockproject_tuanna21.base.BaseFragment;
+import com.tuanna21.mockproject_tuanna21.databinding.FragmentAlbumBinding;
+import com.tuanna21.mockproject_tuanna21.screen.main.viewmodel.MainActivityViewModel;
+
+public class AlbumFragment extends BaseFragment<MainActivityViewModel, FragmentAlbumBinding> {
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initViewModel() {
+        mViewModel = new ViewModelProvider(mActivity).get(MainActivityViewModel.class);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_album;
+    }
+
+    @Override
+    protected void initYourView() {
+
+    }
 }
