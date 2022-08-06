b6b4984f6aa7bf930ade8bc9d7932fdfc85045fd
Jhin-247 <anhtuana1408@gmail.com>
2022-08-06 18:33:39 +0700

    change workflow(room -> device)

--- app/src/main/java/com/tuanna21/mockproject_tuanna21/screen/main/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/tuanna21/mockproject_tuanna21/screen/main/viewmodel/MainActivityViewModel.java
@@ -1,82 +1,37 @@
 package com.tuanna21.mockproject_tuanna21.screen.main.viewmodel;
 
-import static android.content.pm.PackageManager.PERMISSION_GRANTED;
-import static com.tuanna21.mockproject_tuanna21.utils.Constants.SharedPref.HAS_DATABASE;
-
-import android.Manifest;
 import android.app.Application;
-import android.content.Context;
-import android.util.Log;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.net.Uri;
 
-import androidx.core.content.ContextCompat;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.tuanna21.mockproject_tuanna21.R;
-import com.tuanna21.mockproject_tuanna21.db.model.NavigationItem;
-import com.tuanna21.mockproject_tuanna21.db.model.Song;
-import com.tuanna21.mockproject_tuanna21.db.repository.SongRepository;
-import com.tuanna21.mockproject_tuanna21.utils.SharedPreferencesUtils;
-import com.tuanna21.mockproject_tuanna21.utils.SongUtils;
+import com.tuanna21.mockproject_tuanna21.data.model.NavigationItem;
+import com.tuanna21.mockproject_tuanna21.data.model.Song;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivityViewModel extends AndroidViewModel {
-    private final SongRepository mSongRepository;
-    private LiveData<List<Song>> mSongs;
-    private int mScreenHeight;
-    private int mScreenWidth;
-
+    //    private final SongRepository mSongRepository;
+    private final MutableLiveData<List<Song>> mSongs;
 
     public MainActivityViewModel(Application application) {
         super(application);
         mSongs = new MutableLiveData<>();
-        mSongRepository = new SongRepository(application);
-        mSongs = mSongRepository.getSongs();
+//        mSongRepository = new SongRepository(application);
+//        mSongs = mSongRepository.getSongs();
     }
 
     public LiveData<List<Song>> getSongs() {
         return mSongs;
     }
 
-    public int getScreenHeight() {
-        return mScreenHeight;
-    }
-
-    public void setScreenHeight(int screenHeight) {
-        this.mScreenHeight = screenHeight;
-    }
-
-    public int getScreenWidth() {
-        return mScreenWidth;
-    }
-
-    public void setScreenWidth(int screenWidth) {
-        this.mScreenWidth = screenWidth;
-    }
-
-    public boolean checkPermission(Context context) {
-        return ContextCompat.checkSelfPermission(
-                context.getApplicationContext(),
-                Manifest.permission.READ_CONTACTS
-        ) == PERMISSION_GRANTED;
-    }
-
-    public boolean isFirstTimeInit(Context context) {
-        return !SharedPreferencesUtils.getInstance(context.getApplicationContext()).hasDatabaseData();
-    }
-
-    public void requestSyncSongData(Context context) {
-        Log.i("requestSyncSongData", "requestSyncSongData: " + context);
-        List<Song> songList = SongUtils.getAllSongFromDevice(context);
-        for (Song song : songList) {
-            mSongRepository.insertSong(song);
-        }
-        SharedPreferencesUtils.getInstance(context).saveBoolean(HAS_DATABASE, true);
-    }
 
     public List<NavigationItem> getNavigationItems() {
         List<NavigationItem> navigationItems = new ArrayList<>();
@@ -101,5 +56,30 @@ public class MainActivityViewModel extends AndroidViewModel {
         navigationItems.add(new NavigationItem(getApplication().getApplicationContext().getString(R.string.other), R.drawable.ic_other));
 
         return navigationItems;
+    }
+
+    public void setSongCursor(Cursor cursor) {
+        List<Song> songList = new ArrayList<>();
+        if (cursor != null && cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            long time = System.currentTimeMillis();
+            while (cursor.moveToNext()) {
+                Uri imageUri = Uri.parse("content://media/external/audio/albumart");
+                Uri imagePathUri = ContentUris.withAppendedId(imageUri, cursor.getLong(5));
+
+                songList.add(new Song(
+                        cursor.getInt(0),
+                        cursor.getString(1),
+                        String.valueOf(cursor.getLong(2)),
+                        cursor.getString(3),
+                        cursor.getString(4),
+                        imagePathUri.toString(),
+                        cursor.getLong(5),
+                        cursor.getString(6),
+                        time
+                ));
+            }
+            mSongs.setValue(songList);
+        }
     }
 }
