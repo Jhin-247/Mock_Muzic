7fb8563730fb21930f4568113c06d9912f667200
Jhin-247 <anhtuana1408@gmail.com>
2022-08-12 17:20:39 +0700

    add genres drawables

---
+++ app/src/main/java/com/tuanna21/mockproject_tuanna21/screen/main/fragments/songfragments/subfragment/genresfragment/GenresFragment.java
@@ -0,0 +1,42 @@
+package com.tuanna21.mockproject_tuanna21.screen.main.fragments.songfragments.subfragment;
+
+import com.tuanna21.mockproject_tuanna21.base.BaseFragment;
+import com.tuanna21.mockproject_tuanna21.databinding.FragmentGenresBinding;
+import com.tuanna21.mockproject_tuanna21.screen.main.viewmodel.MainViewModel;
+
+public class GenresFragment extends BaseFragment<MainViewModel, FragmentGenresBinding> {
+    @Override
+    protected void initData() {
+        
+    }
+
+    @Override
+    protected void initToolbar() {
+
+    }
+
+    @Override
+    protected void initObserver() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initViewModel() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initYourView() {
+
+    }
+}
